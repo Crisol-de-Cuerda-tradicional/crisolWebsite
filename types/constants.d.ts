015860fecbbe6f98f53d812e6d042c348d3a762e
Ishdril <berni2015@gmail.com>
2022-04-18 12:49:52 +0200

    feat: import yaml files and typescript support

---
+++ types/constants.d.ts
@@ -0,0 +1,53 @@
+declare module '*config.yml' {
+  interface Config {
+    name: string;
+    nameShort: string;
+    startDate: Date;
+    endDate: Date;
+    registrationOpen: boolean;
+    teachers: {
+      fiddle: string[];
+      cello: string[];
+      guitar: string[];
+      flute: string[];
+      voice: string[];
+      dance: string[];
+      houseBand: string[];
+      pendingTeachers: boolean;
+    };
+    socialMedia: {
+      instagram: string;
+      facebook: string;
+      vimeo: string;
+      youtube: string;
+      spotify: string;
+    };
+  }
+  const config: Config;
+  export default config;
+}
+
+declare module '*crisolBookIndex.yml' {
+  interface CrisolBookEntry {
+    title: string;
+    author: string;
+    origin: string;
+  }
+
+  interface CrisolBookIndex {
+    lastUpdated: Date;
+    index: CrisolBookEntry[];
+  }
+  const crisolBoookIndex: CrisolBookIndex;
+  export default crisolBoookIndex;
+}
+
+declare module '*teachers.yml' {
+  interface Teacher {
+    id: string;
+    name: string;
+    years: number[];
+  }
+
+  export const teachers: Teacher[];
+}
