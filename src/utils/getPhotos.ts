0a81543558e57e85260d9c5b08272ada4d60c84b
bernatduran <bduran@ohpen.com>
2023-09-21 03:41:49 +0200

    fix: google photos access

--- src/utils/getPhotos.ts
+++ src/utils/getPhotos.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getPhotos = async (albumId: string): Promise<string[]> => {
-  const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g;
+  const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9\-_]*)"/g;
 
   function extractPhotos(content: any): string[] {
     const links = new Set<string>();
@@ -13,9 +13,14 @@ export const getPhotos = async (albumId: string): Promise<string[]> => {
   }
 
   async function getAlbum(albumId: string): Promise<string[]> {
-    const response = await axios.get(`https://photos.app.goo.gl/${albumId}`);
-    const photos = extractPhotos(response.data);
-    return photos;
+    try {
+      const response = await axios.get(`https://photos.app.goo.gl/${albumId}`);
+      const photos = extractPhotos(response.data);
+      return photos;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   }
 
   return getAlbum(albumId);
