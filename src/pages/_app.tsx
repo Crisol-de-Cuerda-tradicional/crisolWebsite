ec42ee789d974c24bf11cc621271ca2d6ba4f0cf
Ishdril <berni2015@gmail.com>
2024-03-24 16:04:19 +0100

    ci: migrate CD workflow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,6 @@ import { useLocale } from '@hooks';
 
 import '../components/Navbar/Burger.scss';
 import '@styles/globals.css';
-import { useLocale } from '@hooks';
 
 config.autoAddCss = false;
 
