764902b981f9143133247259983c7771d23dfc34
Ishdril <berni2015@gmail.com>
2024-04-03 23:22:37 +0200

    fix: prevent empty language selection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export const useRedirect = (path?: string) => {
 
   // language detection
   useEffect(() => {
-    const detectedLng = languageDetector.detect() ?? '';
+    const detectedLng = languageDetector.detect() ?? 'es';
     if (to.startsWith('/' + detectedLng) && router.route === '/404') {
       // prevent endless loop
       router.replace('/' + detectedLng + router.route);
