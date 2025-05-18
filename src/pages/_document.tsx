1fb2f704d916b555617181be6f759de77472eabd
Bernat Duran <bernatd@spacelift.io>
2025-05-18 16:06:10 +0200

    fix: fonts preconnect crossorigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,7 +22,7 @@ class MyDocument extends Document {
             content="Crisol de Cuerda is a traditional violin, cello, guitar and fulte music camp celebrated in Spain"
           />
           <link rel="icon" href={baseUrl('/favicon.png')} />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           {this.shouldLoadVideo ? (
             <link
               rel="preload"
