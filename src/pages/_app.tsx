96035916ffa6a5d12b34461d0b2a6187ab6351f6
Ishdril <berni2015@gmail.com>
2026-02-10 23:27:05 +0100

    test: staging environment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,30 @@
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import dayjs from 'dayjs';
-import 'dayjs/locale/en';
-import 'dayjs/locale/es';
-import advancedFormat from 'dayjs/plugin/advancedFormat';
-import LocalizedFormat from 'dayjs/plugin/localizedFormat';
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
-import { Montserrat } from 'next/font/google';
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import dayjs from "dayjs";
+import "dayjs/locale/en";
+import "dayjs/locale/es";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import LocalizedFormat from "dayjs/plugin/localizedFormat";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import { Montserrat } from "next/font/google";
 
-import { Layout, MenuContextProvider } from '@components';
-import { useLocale } from '@hooks';
+import { Layout, MenuContextProvider } from "@components";
+import { useLocale } from "@hooks";
 
-import '@styles/globals.css';
-import '../components/Navbar/Burger.scss';
+import "@styles/globals.css";
+import "../components/Navbar/Burger.scss";
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: '--font-headings',
-  weight: ['300', '700'],
-  display: 'swap',
+  subsets: ["latin"],
+  variable: "--font-headings",
+  weight: ["300", "700"],
+  display: "swap",
   preload: true,
-  style: ['normal'],
-  fallback: ['sans-serif'],
+  style: ["normal"],
+  fallback: ["sans-serif"],
 });
 config.autoAddCss = false;
 
@@ -39,6 +39,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <MenuContextProvider>
+      {/* This is to check the staging environment */}
+      <div>
+        <h1>Staging Environment</h1>
+        <p>This is a staging environment</p>
+      </div>
       <main className={montserrat.variable}>
         <Layout>
           <Component {...pageProps} />
